4ee1d26692c0a7f9e1bcd8c2e550b8cbd6faa5b1
Jahidur Rahman <jahidur011@gmail.com>
2026-06-13 13:38:37 +0600

    more practice of concepts

---
+++ tests/multidropdown.spec.js
@@ -0,0 +1,21 @@
+const {test, expect} = require('@playwright/test')
+
+test('Multi Select DropDown',async({page})=>{
+    await page.goto('https://testautomationpractice.blogspot.com/')
+    await page.locator("#colors").selectOption(['Blue', 'Green', 'Yellow'])
+
+    //validation of count and colors in the dropdown
+    await expect(page.locator('#colors option')).toHaveCount(7)
+
+    const expectedColors = [
+        "Red",
+        "Blue",
+        "Green",
+        "Yellow",
+        "Red",
+        "White",
+        "Green"
+    ]
+    await expect(page.locator('#colors option')).toHaveText(expectedColors)
+    
+})
